0e82bb96e77041f0c7362c25203bf3c82181f998
suvancha9866 <suvan.chatakondu@gmail.com>
2025-05-28 00:34:07 -0500

    Worked on Constraints Function and Added Tests

---
+++ dede/constraints_func.py
@@ -0,0 +1,76 @@
+import cvxpy as cp
+import numpy as np
+from cvxpy.constraints.zero import Equality
+from cvxpy.constraints.nonpos import Inequality
+from cvxpy.atoms.affine.sum import Sum
+from cvxpy.expressions.constants.constant import Constant
+from cvxpy.atoms.affine.add_expr import AddExpression
+from cvxpy.atoms.affine.binary_operators import MulExpression
+from cvxpy.expressions.variable import Variable
+
+def func(constr):
+    if type(constr) == list or type(constr) == tuple:
+        out = []
+        for c in constr:
+            out.extend(func(c))
+        return out
+    left, right = constr.args
+    constr_list = []
+    left_list = []
+    right_list = []
+    print("Left side:", left)
+    print("Right side:", right)
+    print("Type of left side:", type(left))
+    print("Type of right side:", type(right))
+    print("left value:", left.value)
+    print("Type of left value:", type(left.value))
+    print("right value:", right.value)
+    print("Type of right value:", type(right.value))
+    if type(constr) == Inequality:
+        if type(left) == Sum:
+            for i in range(left.shape[0]):
+                left_list.append(left.args[0][i])
+        elif type(left) == Variable:
+            left_list.append(left)
+        elif type(left.value) == np.ndarray:
+            for idx in np.ndindex(left.shape):
+                left_list.append(left[idx])
+        elif left.value != None and type(float(left.value)) == float:
+            left_list.append(float(left.value))
+        elif type(left.value) == list or type(left.value) == np.ndarray:
+            for v in left.value:
+                left_list.append(float(v))
+        # elif type(left) == AddExpression:
+        #     need to finish
+        else:
+            raise TypeError(f"Left side of the constraint is not a valid type: {type(left)}")
+        
+        if type(right) == Sum:
+            for i in range(right.shape[0]):
+                right_list.append(right.args[0][i])
+        elif type(right) == Variable:
+            right_list.append(right)
+        elif type(right.value) == np.ndarray:
+            for idx in np.ndindex(right.shape):
+                right_list.append(right[idx])  
+        elif right.value != None and type(float(right.value)) == float:
+            right_list.append(float(right.value))
+        elif type(list(right.value)) == list or type(right.value) == np.ndarray:
+            for v in right.value:
+                right_list.append(float(v))
+        # elif type(right) == AddExpression:
+        #     Need to finish
+        else:
+            raise TypeError(f"Right side of the constraint is not a valid type: {type(right)}")
+    else:
+        raise TypeError("Only Inequality based Testing.")
+    if len(left_list) < len(right_list):
+        for i in range(len(right_list)):
+            constr_list.append(left_list[0] <= right_list[i])
+    elif len(left_list) > len(right_list):
+        for i in range(len(left_list)):
+            constr_list.append(left_list[i] <= right_list[0])
+    elif len(left_list) == len(right_list):
+        for i in range(len(left_list)):
+            constr_list.append(left_list[i] <= right_list[i])
+    return constr_list
---
+++ tests/tests_suvan.py
@@ -0,0 +1,87 @@
+import numpy as np
+import cvxpy as cp
+import pytest
+from constraints_func import func
+
+def tester(constr):
+    return [str(c) for c in constr]
+
+def test_sample():
+    x = cp.Variable((4,4))
+    constr = x.sum(1) <= 1
+    out = func(constr)
+    expected = [str(x[i, :] <= 1) for i in range(4)]
+    assert tester(out) == expected
+
+def test_failing_sample():
+    x = cp.Variable((4,4))
+    constr = x.sum(1) == 1
+    with pytest.raises(TypeError):
+        func(constr)
+
+def test_hard1():
+    x = cp.Variable((4,4))
+    constr = x.sum(1) >= np.arange(4)
+    out = func(constr)
+    expected = [str(x[i, :] >= i) for i in range(4)]
+    assert tester(out) == expected
+
+
+def test_extra1():
+    x = cp.Variable()
+    constr = x <= 5
+    out = func(constr)
+    expected = [str(x <= 5)]
+    assert tester(out) == expected
+
+def test_failing_extra1():
+    x = cp.Variable()
+    constr = x == 5
+    with pytest.raises(TypeError):
+        func(constr)
+
+def test_extra2():
+    x = cp.Variable((4,4))
+    constr = x[0,:] >= np.arange(4)
+    out = func(constr)
+    expected = [str(x[i,1] >= i) for i in range(4)]
+    assert tester(out) == expected
+
+def test_failing_extra2():
+    x = cp.Variable((4,4))
+    constr = x[:,1] == np.arange(4)
+    with pytest.raises(TypeError):
+        func(constr)
+
+
+
+
+
+
+# def test_hard2(): #Not fully implemented
+#     x = cp.Variable((4,4))
+#     c1 = x[0,2:] + x[2,3] >= 10
+#     c2 = x[2:] @ np.arange(5,9) >= 0
+#     out = []
+#     out.append(func(c1)) 
+#     out.append(func(c2))
+#     print(tester(out))
+#     expected = [x[0,2] + x[0,3] + x[2,3] >= 10, x[2,:] @ np.arange(5,9) >= 0, x[3,:] @ np.arange(5,9) >= 0]
+#     assert tester(out) == tester(expected)
+
+# def test_hard3(): #Not fully implemented
+#     x = cp.Variable((4,4))
+#     c1 = x.sum(1) >= np.arange(4)
+#     c2 = x[0,0] + x[0,1] >= 0
+#     out = []
+#     out.append(func(c1)) 
+#     out.append(func(c2))
+#     print(tester(out))
+#     expected = [x[i, :] >= i for i in range(4)] + [x[0,0] + x[0,1] >= 0]
+#     assert tester(out) == tester(expected)
+
+# # def test_hard4():
+# #     x = cp.Variable((4,4))
+# #     out = dd.trace(x)
+# #     expected = [x[i, i] <= for i in range(4)]
+    
