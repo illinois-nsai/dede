1720cf2a596be82cb578f75035f089c9c2deeecb
suvancha9866 <suvan.chatakondu@gmail.com>
2025-05-30 23:53:42 -0500

    Added AddExpression, x[0,:], and x.sum(0) Constraints

--- tests/tests_suvan.py
+++ tests/tests_suvan.py
@@ -13,6 +13,13 @@ def test_sample():
     expected = [str(x[i, :] <= 1) for i in range(4)]
     assert tester(out) == expected
 
+def test_sample_2():
+    x = cp.Variable((4,4))
+    constr = x.sum(0) <= 1
+    out = func(constr)
+    expected = [str(x[:, i] <= 1) for i in range(4)]
+    assert tester(out) == expected
+
 def test_failing_sample():
     x = cp.Variable((4,4))
     constr = x.sum(1) == 1
@@ -42,18 +49,19 @@ def test_failing_extra1():
     expected = [str(x == 5)]
     assert tester(out) == expected
 
-def test_extra2(): # Haven't configured
+def test_extra2():
     x = cp.Variable((4,4))
     constr = x[0,:] >= np.arange(4)
     out = func(constr)
-    expected = [str(x[i,1] >= i) for i in range(4)]
+    expected = [str(x[0,i] >= i) for i in range(4)]
     assert tester(out) == expected
 
 def test_failing_extra2():
     x = cp.Variable((4,4))
     constr = x[:,1] == np.arange(4)
-    with pytest.raises(TypeError):
-        func(constr)
+    out = func(constr)
+    expected = [str(x[i,1] == i) for i in range(4)]
+    assert tester(out) == expected
 
 
 
@@ -63,24 +71,24 @@ def test_failing_extra2():
 # def test_hard2(): #Not fully implemented
 #     x = cp.Variable((4,4))
 #     c1 = x[0,2:] + x[2,3] >= 10
-#     c2 = x[2:] @ np.arange(5,9) >= 0
+#     c2 = x.sum(1) >= x[2:] @ np.arange(5,9) >= 0
 #     out = []
-#     out.append(func(c1)) 
-#     out.append(func(c2))
+#     out.extend(func(c1)) 
+#     out.extend(func(c2))
 #     print(tester(out))
 #     expected = [x[0,2] + x[0,3] + x[2,3] >= 10, x[2,:] @ np.arange(5,9) >= 0, x[3,:] @ np.arange(5,9) >= 0]
 #     assert tester(out) == tester(expected)
 
-# def test_hard3(): #Not fully implemented
-#     x = cp.Variable((4,4))
-#     c1 = x.sum(1) >= np.arange(4)
-#     c2 = x[0,0] + x[0,1] >= 0
-#     out = []
-#     out.append(func(c1)) 
-#     out.append(func(c2))
-#     print(tester(out))
-#     expected = [x[i, :] >= i for i in range(4)] + [x[0,0] + x[0,1] >= 0]
-#     assert tester(out) == tester(expected)
+def test_hard3():
+    x = cp.Variable((4,4))
+    c1 = x.sum(1) >= np.arange(4)
+    c2 = x[0,0] + x[0,1] >= 0
+    out = []
+    out.extend(func(c1)) 
+    out.extend(func(c2))
+    print(tester(out))
+    expected = [x[i, :] >= i for i in range(4)] + [x[0,0] + x[0,1] >= 0]
+    assert tester(out) == tester(expected)
 
 # # def test_hard4():
 # #     x = cp.Variable((4,4))
