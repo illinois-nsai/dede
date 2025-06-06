be7fda4e75834b67ce27df4591612a1d51b4bcd6
suvancha9866 <suvan.chatakondu@gmail.com>
2025-06-06 15:44:34 -0500

    Formatted Suvan's Code

--- dede/constraints_func.py
+++ dede/constraints_func.py
@@ -1,17 +1,16 @@
-import cvxpy as cp
 import numpy as np
 from cvxpy.constraints.zero import Equality
 from cvxpy.constraints.nonpos import Inequality
 from cvxpy.atoms.affine.sum import Sum
 from cvxpy.expressions.constants.constant import Constant
 from cvxpy.atoms.affine.add_expr import AddExpression
-from cvxpy.atoms.affine.binary_operators import MulExpression
 from cvxpy.expressions.variable import Variable
 from cvxpy.atoms.affine.index import index
 from cvxpy.atoms.affine.promote import Promote
 
+
 def func(constr):
-    if type(constr) == list or type(constr) == tuple:
+    if isinstance(constr, (list, tuple)):
         out = []
         for c in constr:
             out.extend(func(c))
@@ -20,23 +19,15 @@ def func(constr):
     constr_list = []
     left_list = []
     right_list = []
-    # print("Left side:", left)
-    # print("Right side:", right)
-    # print("Type of left side:", type(left))
-    # print("Type of right side:", type(right))
-    # print("left value:", left.value)
-    # print("Type of left value:", type(left.value))
-    # print("right value:", right.value)
-    # print("Type of right value:", type(right.value))
-    if type(constr) == Inequality or type(constr) == Equality:
-        if type(left) == Sum:
+    if isinstance(constr, (Equality, Inequality)):
+        if isinstance(left, Sum):
             if left.axis == 0:
                 for i in range(left.shape[0]):
                     left_list.append(left.args[0][:, i])
             elif left.axis == 1:
                 for i in range(left.shape[0]):
                     left_list.append(left.args[0][i])
-        elif type(left) == AddExpression:
+        elif isinstance(left, AddExpression):
             expr_list = []
             for expr in left.args:
                 expr_list.extend(breakdown_expression(expr))
@@ -44,27 +35,26 @@ def func(constr):
             for t in expr_list[1:]:
                 combined_left = combined_left + t
             left_list.append(combined_left)
-        
-        elif type(left) == Variable:
+        elif isinstance(left, Variable):
             left_list.append(left)
-        elif type(left) == Constant and type(left.value) == np.ndarray:
+        elif isinstance(left, Constant) and isinstance(left.value, np.ndarray):
             for i in np.ndindex(left.value.shape):
                 left_list.append(float(left.value[i]))
-        elif type(left.value) == np.ndarray or type(left) == index:
+        elif isinstance(left.value, np.ndarray) or isinstance(left, index):
             for i in np.ndindex(left.shape):
-                left_list.append(left[i]) # if it is a float, then after the fact maybe cast it to a float
-        elif left.value != None and type(float(left.value)) == float:
+                left_list.append(left[i])  # Cast to Float if Needed
+        elif left.value is not None and isinstance(float(left.value), float):
             left_list.append(float(left.value))
-        elif type(left.value) == list or type(left.value) == np.ndarray:
+        elif isinstance(list(left.value), (list, np.ndarray)):
             for v in left.value:
                 left_list.append(float(v))
         else:
-            raise TypeError(f"Left side of the constraint is not a valid type: {type(left)}")
-        
-        if type(right) == Sum:
+            raise TypeError(f"Left Expression Not Supported: {type(left)}")
+
+        if isinstance(right, Sum):
             for i in range(right.shape[0]):
                 right_list.append(right.args[0][i])
-        elif type(right) == AddExpression:
+        elif isinstance(right, AddExpression):
             expr_list = []
             for expr in right.args:
                 expr_list.extend(breakdown_expression(expr))
@@ -72,66 +62,71 @@ def func(constr):
             for t in expr_list[1:]:
                 combined_right = combined_right + t
             right_list.append(combined_right)
-        elif type(right) == Variable:
+        elif isinstance(right, Variable):
             right_list.append(right)
-        elif type(right) == Constant and type(right.value) == np.ndarray:
+        elif isinstance(right, Constant) and isinstance(right.value, np.ndarray):
             for i in np.ndindex(right.value.shape):
                 right_list.append(float(right.value[i]))
-        elif type(right.value) == np.ndarray or type(right) == index:
+        elif isinstance(right.value, np.ndarray) or isinstance(right, index):
             for i in np.ndindex(right.shape):
-                right_list.append(right[i])  # if it is a float, then after the fact maybe cast it to a float
-        elif right.value != None and type(float(right.value)) == float:
+                right_list.append(right[i])  # Cast to Float if Needed
+        elif right.value is not None and isinstance(float(right.value), float):
             right_list.append(float(right.value))
-        elif type(list(right.value)) == list or type(right.value) == np.ndarray:
+        elif isinstance(list(right.value), list):
             for v in right.value:
                 right_list.append(float(v))
         else:
-            raise TypeError(f"Right side of the constraint is not a valid type: {type(right)}")
+            raise TypeError(f"Right Expression Not Supported: {type(right)}")
     else:
         raise TypeError("Only Inequality or Equality based Testing.")
-    print("Left List:", left_list)
-    print("Right List:", right_list)
-    if len(left_list) < len(right_list) and type(constr) == Inequality:
-        for i in range(len(right_list)):
-            constr_list.append(left_list[0] <= right_list[i])
-    elif len(left_list) > len(right_list) and type(constr) == Inequality:
-        for i in range(len(left_list)):
-            constr_list.append(left_list[i] <= right_list[0])
-    elif len(left_list) == len(right_list) and type(constr) == Inequality:
-        for i in range(len(left_list)):
-            constr_list.append(left_list[i] <= right_list[i])
-    elif len(left_list) < len(right_list) and type(constr) == Equality:
-        for i in range(len(right_list)):
-            constr_list.append(left_list[0] == right_list[i])
-    elif len(left_list) > len(right_list) and type(constr) == Equality:
-        for i in range(len(left_list)):
-            constr_list.append(left_list[i] == right_list[0])
-    elif len(left_list) == len(right_list) and type(constr) == Equality:
-        for i in range(len(left_list)):
-            constr_list.append(left_list[i] == right_list[i])
+
+    if isinstance(constr, Inequality):
+        if len(left_list) < len(right_list):
+            for i in range(len(right_list)):
+                constr_list.append(left_list[0] <= right_list[i])
+        elif len(left_list) > len(right_list):
+            for i in range(len(left_list)):
+                constr_list.append(left_list[i] <= right_list[0])
+        elif len(left_list) == len(right_list):
+            for i in range(len(left_list)):
+                constr_list.append(left_list[i] <= right_list[i])
+
+    elif isinstance(constr, Equality):
+        if len(left_list) < len(right_list):
+            for i in range(len(right_list)):
+                constr_list.append(left_list[0] == right_list[i])
+        elif len(left_list) > len(right_list):
+            for i in range(len(left_list)):
+                constr_list.append(left_list[i] == right_list[0])
+        elif len(left_list) == len(right_list):
+            for i in range(len(left_list)):
+                constr_list.append(left_list[i] == right_list[i])
+
     return constr_list
+
 
 def breakdown_expression(expr):
     expr_list = []
-    if type(expr) == Sum:
+    if isinstance(expr, Sum):
         for i in range(expr.shape[0]):
             expr_list.append(expr.args[0][i])
-    elif type(expr) == AddExpression:
-            for addend in expr.args:
-                subterms = breakdown_expression(addend)
-                expr_list.extend(subterms)
-    elif type(expr) == Variable:
+    elif isinstance(expr, AddExpression):
+        for addend in expr.args:
+            subterms = breakdown_expression(addend)
+            expr_list.extend(subterms)
+    elif isinstance(expr, Variable):
         expr_list.append(expr)
-    elif type(expr) == Promote:
+    elif isinstance(expr, Promote):
         expr_list.extend(breakdown_expression(expr.args[0]))
-    elif type(expr.value) == np.ndarray or type(expr) == index:
+    elif isinstance(expr.value, np.ndarray) or isinstance(expr, index):
         for i in np.ndindex(expr.shape):
-            expr_list.append(expr[i]) # if it is a float, then after the fact maybe cast it to a float
-    elif expr.value != None and type(float(expr.value)) == float:
+            expr_list.append(expr[i])  # Cast to Float if Needed
+    elif expr.value is not None and isinstance(float(expr.value), float):
         expr_list.append(float(expr.value))
-    elif type(expr.value) == list or type(expr.value) == np.ndarray:
+    elif isinstance(expr.value, (list, np.ndarray)):
         for v in expr.value:
             expr_list.append(float(v))
     else:
         raise TypeError(f"In Breakdown Expression: {type(expr)}")
+
     return expr_list
--- tests/tests_suvan.py
+++ tests/tests_suvan.py
@@ -1,34 +1,34 @@
 import numpy as np
 import cvxpy as cp
-import pytest
 from constraints_func import func
 
-def tester(constr):
-    return [str(c) for c in constr]
 
 def test_sample():
-    x = cp.Variable((4,4))
+    x = cp.Variable((4, 4))
     constr = x.sum(1) <= 1
     out = func(constr)
     expected = [str(x[i, :] <= 1) for i in range(4)]
     assert tester(out) == expected
 
+
 def test_sample_2():
-    x = cp.Variable((4,4))
+    x = cp.Variable((4, 4))
     constr = x.sum(0) <= 1
     out = func(constr)
     expected = [str(x[:, i] <= 1) for i in range(4)]
     assert tester(out) == expected
 
+
 def test_failing_sample():
-    x = cp.Variable((4,4))
+    x = cp.Variable((4, 4))
     constr = x.sum(1) == 1
     out = func(constr)
     expected = [str(x[i, :] == 1) for i in range(4)]
     assert tester(out) == expected
 
+
 def test_hard1():
-    x = cp.Variable((4,4))
+    x = cp.Variable((4, 4))
     constr = x.sum(1) >= np.arange(4)
     out = func(constr)
     expected = [str(x[i, :] >= i) for i in range(4)]
@@ -42,6 +42,7 @@ def test_extra1():
     expected = [str(x <= 5)]
     assert tester(out) == expected
 
+
 def test_failing_extra1():
     x = cp.Variable()
     constr = x == 5
@@ -49,49 +50,46 @@ def test_failing_extra1():
     expected = [str(x == 5)]
     assert tester(out) == expected
 
+
 def test_extra2():
-    x = cp.Variable((4,4))
-    constr = x[0,:] >= np.arange(4)
+    x = cp.Variable((4, 4))
+    constr = x[0, :] >= np.arange(4)
     out = func(constr)
-    expected = [str(x[0,i] >= i) for i in range(4)]
+    expected = [str(x[0, i] >= i) for i in range(4)]
     assert tester(out) == expected
+
 
 def test_failing_extra2():
-    x = cp.Variable((4,4))
-    constr = x[:,1] == np.arange(4)
+    x = cp.Variable((4, 4))
+    constr = x[:, 1] == np.arange(4)
     out = func(constr)
-    expected = [str(x[i,1] == i) for i in range(4)]
+    expected = [str(x[i, 1] == i) for i in range(4)]
     assert tester(out) == expected
 
 
-
-
+def test_hard3():
+    x = cp.Variable((4, 4))
+    c1 = x.sum(1) >= np.arange(4)
+    c2 = x[0, 0] + x[0, 1] >= 0
+    out = []
+    out.extend(func(c1))
+    out.extend(func(c2))
+    print(tester(out))
+    expected = [x[i, :] >= i for i in range(4)] + [x[0, 0] + x[0, 1] >= 0]
+    assert tester(out) == tester(expected)
 
 
 # def test_hard2(): #Not fully implemented
 #     x = cp.Variable((4,4))
 #     c1 = x[0,2:] + x[2,3] >= 10
-#     c2 = x.sum(1) >= x[2:] @ np.arange(5,9) >= 0
+#     c2 = x[2:] @ np.arange(5,9) >= 0
 #     out = []
-#     out.extend(func(c1)) 
+#     out.extend(func(c1))
 #     out.extend(func(c2))
 #     print(tester(out))
 #     expected = [x[0,2] + x[0,3] + x[2,3] >= 10, x[2,:] @ np.arange(5,9) >= 0, x[3,:] @ np.arange(5,9) >= 0]
 #     assert tester(out) == tester(expected)
 
-def test_hard3():
-    x = cp.Variable((4,4))
-    c1 = x.sum(1) >= np.arange(4)
-    c2 = x[0,0] + x[0,1] >= 0
-    out = []
-    out.extend(func(c1)) 
-    out.extend(func(c2))
-    print(tester(out))
-    expected = [x[i, :] >= i for i in range(4)] + [x[0,0] + x[0,1] >= 0]
-    assert tester(out) == tester(expected)
 
-# # def test_hard4():
-# #     x = cp.Variable((4,4))
-# #     out = dd.trace(x)
-# #     expected = [x[i, i] <= for i in range(4)]
-    
+def tester(constr):
+    return [str(c) for c in constr]
