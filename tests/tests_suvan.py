9fe6d45e2ed90ce081ab3c8300ea99fb65c3032d
lkhsiao2 <lkhsiao2@node0.lkhsiao2-260388.insai-pg0.utah.cloudlab.us>
2025-06-15 23:03:52 -0600

    refactored constraints_utils.py code and added more tests

--- tests/tests_suvan.py
+++ tests/tests_suvan.py
@@ -86,16 +86,58 @@ def test_column():
     assert tester(out) == expected
 
 
-# def test_hard2(): #Not fully implemented
-#     x = cp.Variable((4,4))
-#     c1 = x[0,2:] + x[2,3] >= 10
-#     c2 = x[2:] @ np.arange(5,9) >= 0
-#     out = []
-#     out.extend(func(c1))
-#     out.extend(func(c2))
-#     print("GOT:", tester(out))
-#     expected = [x[0,2] + x[0,3] + x[2,3] >= 10, x[2,:] @ np.arange(5,9) >= 0, x[3,:] @ np.arange(5,9) >= 0]
-#     assert tester(out) == tester(expected)
+def test_2d_chunk():
+    x = cp.Variable((4, 4))
+    constr = x[:2, 1::2] >= np.arange(4).reshape((2, 2))
+    out = func(constr)
+    expected = [
+        str(x[0, 1] >= 0), str(x[0, 3] >= 1),
+        str(x[1, 1] >= 2), str(x[1, 3] >= 3)
+    ]
+    assert tester(out) == expected
+
+
+def test_single_row():
+    x = cp.Variable(4)
+    constr = x[:] >= np.arange(4)
+    out = func(constr)
+    expected = [str(x[i] >= i) for i in range(4)]
+    assert tester(out) == expected
+
+
+def test_1d_sum():
+    x = cp.Variable(4)
+    constr = x.sum() >= 1
+    out = func(constr)
+    assert str(out[0]) == str(constr)
+
+
+def test_2d_sum():
+    x = cp.Variable((4, 4))
+    constr = x.sum() >= 1
+    out = func(constr)
+    assert str(out[0]) == str(constr)
+
+
+def test_sum_vars():
+    x = cp.Variable((4, 4))
+    y = cp.Variable((4, 4))
+    constr = x + y + 1 == 0
+    out = func(constr)
+    expected = [x[tup] + y[tup] + 1 == 0 for tup in np.ndindex(x.shape)]
+    assert tester(out) == tester(expected)
+
+
+def test_hard2(): #Not fully implemented
+    x = cp.Variable((4,4))
+    c1 = x[0,2:] + x[2,3] >= 10
+    c2 = x[2:] @ np.arange(5,9) >= 0
+    out = []
+    out.extend(func(c1))
+    out.extend(func(c2))
+    print("GOT:", tester(out))
+    expected = [x[0,2] + x[0,3] + x[2,3] >= 10, x[2,:] @ np.arange(5,9) >= 0, x[3,:] @ np.arange(5,9) >= 0]
+    assert tester(out) == tester(expected)
 
 
 def tester(constr):
@@ -111,4 +153,11 @@ if __name__ == '__main__':
     test_extra2()
     test_failing_extra2()
     test_hard3()
+
     test_column()
+    test_2d_chunk()
+    test_single_row()
+    #test_hard2()
+    test_1d_sum()
+    test_2d_sum()
+    test_sum_vars()
