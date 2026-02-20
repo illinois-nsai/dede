bbae99242a9289c48e5bf5e38a6a54dd0683e572
Zhang, Ian <yianzhang14@gmail.com>
2026-02-19 23:25:01 -0600

    update test script

--- tmp_tests/parallel.py
+++ tmp_tests/parallel.py
@@ -1,10 +1,14 @@
 import csv
 import time
 
+import ray
+
 import dede as dd
 
+ray.init(address="auto")
 
-def test(n: int, num_cpus: int) -> tuple:
+
+def test(n: int) -> tuple:
     N, M = n, n
     x = dd.Variable((N, M), nonneg=True)
     resource_constraints = [x[i, :].sum() >= (i + 1) * M for i in range(N)]
@@ -14,23 +18,19 @@ def test(n: int, num_cpus: int) -> tuple:
 
     prob = dd.Problem(objective, resource_constraints, demand_constraints)
     start = time.time()
-    result_dede = prob.solve(solver=dd.SCS, num_cpus=num_cpus)
+    result_dede = prob.solve(solver=dd.SCS)
     time_dede = time.time() - start
 
-    return (n, num_cpus, time_dede)
-
-    with open("timing.txt", "a") as f:
-        f.write(f"{n} {time_dede} {result_dede}\n")
+    return (n, time_dede)
 
 
 if __name__ == "__main__":
     arr = []
-    for num_cpus in [1, 2, 4, 8, 16, 32]:
-        for i in [10, 50, 100, 200]:
-            print(i, num_cpus)
-            res = test(i, num_cpus)
-            print(res)
-            arr.append(res)
+    for i in [10, 50, 100, 200]:
+        print(i)
+        res = test(i)
+        print(res)
+        arr.append(res)
 
     # Writing to the CSV
     with open("output.csv", "w", newline="") as f:
