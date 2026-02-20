b4e2181f0616caae73b7849f17b2813383491c61
Zhang, Ian <yianzhang14@gmail.com>
2026-02-19 23:12:35 -0600

    parallelize the init

--- tmp_tests/parallel.py
+++ tmp_tests/parallel.py
@@ -26,7 +26,7 @@ def test(n: int, num_cpus: int) -> tuple:
 if __name__ == "__main__":
     arr = []
     for num_cpus in [1, 2, 4, 8, 16, 32]:
-        for i in [100, 500, 1000]:
+        for i in [10, 50, 100, 200]:
             print(i, num_cpus)
             res = test(i, num_cpus)
             print(res)
