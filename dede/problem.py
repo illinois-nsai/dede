55c6a56484f5e7d9740c1575fe752050f75f122f
Zhang, Ian <yianzhang14@gmail.com>
2026-05-07 00:28:22 -0500

    log number of iterations

--- dede/problem.py
+++ dede/problem.py
@@ -485,6 +485,7 @@ class Problem(CpProblem):
 
         end = time.perf_counter()
         print("DeDe Solve Time:", end - start)
+        print("DeDe Iterations:", i)
 
         self.populate_vars_with_solution()
         coeff = 1 if self._problem_type == Minimize else -1
